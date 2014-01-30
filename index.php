bd3d2dc659b261b1d9bad9b8b1b9371f28b60d8f
innaaura <inna.beschastnaya@gmail.com>
2014-01-30 18:38:33 +0200

    test

--- index.php
+++ index.php
@@ -6,5 +6,6 @@ echo "<a href='/phpbasics/callbacks.php'>    1.1. Callbacks</a><br/>";
 echo "<a href='/phpbasics/variables.php'>    1.2. Variables</a><br/>";
 echo "<a href='/phpbasics/operators.php'>    1.3. Operators</a><br/>";
 echo "<a href='/phpbasics/constructions.php'>    1.3. Constructions</a><br/>";
+
 echo "<a href='/.php'>. </a><br/>";
 ?>
